3a39cf080471f82573f225889f90213c38c4cfd0
Luisangel <aarroyoh@unsa.edu.pe>
2022-05-12 18:55:41 -0500

    JetpackCompose  funcion Recepies  implementacion del navegation con jetpackCompose

--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -16,54 +16,82 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImagePainter
 import coil.compose.ImagePainter
 import coil.compose.rememberAsyncImagePainter
 import com.example.lab3.ui.theme.Lab3Theme
+
+
+
+
+
 data class Regiones(
     val title: String,
-    val url: String
+    val url: String,
+    val recepies: String
 )
-val regionesList = listOf(
-    Regiones("Amazonas", "https://www.fundacionaquae.org/wp-content/uploads/2019/07/rio-amazonas.jpg"),
-    Regiones("Áncash","https://www.peru.travel/Contenido/Destino/Imagen/pe/26/1.3/Principal/Ancash.jpg"),
-    Regiones("Apurimac","https://d1bvpoagx8hqbg.cloudfront.net/originals/apurimac-alturas-canones-naturales-fd503277fa8c8610123683d60f483e07.jpg"),
-    Regiones("Arequipa","https://media.vogue.mx/photos/5e5c5b1f25623100081c437c/master/w_3000,h_1999,c_limit/Arequipa--paisaje.jpg"),
-    Regiones("Ayacucho","https://denomades.s3.us-west-2.amazonaws.com/blog/wp-content/uploads/2020/09/13172228/arco-ayacucho.jpg"),
-    Regiones("Cajamarca","https://www.regioncajamarca.gob.pe/media/portal/KJDIG/noticia/Plaza_de_Armas_WEB_1.jpg?r=1573668242"),
-    Regiones("Callao","https://elcomercio.pe/resizer/MfFipsfIj-o5SN8l6-fDYGTRqKY=/980x0/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/EVFPRGZ4W5G5DPOSRSWL2PMUZE.jpg"),
-    Regiones("Cusco","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/33/fc/ba/cusco.jpg?w=700&h=500&s=1"),
-    Regiones("Huancavelica","https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Panor%C3%A1mica_de_Huancavelica.jpg/1200px-Panor%C3%A1mica_de_Huancavelica.jpg"),
-    Regiones("Huánuco","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/4a/85/5e/dscf7752-snapseed-largejpg.jpg?w=500&h=400&s=1"),
-    Regiones("Ica","https://www.peru.travel/Contenido/Destino/Imagen/es/5/1.2/Principal/SouthAmericaPeru2017_1010_180704-5912_AGP_HDR.jpg"),
-    Regiones("Junín","https://la.network/wp-content/uploads/2019/05/huancayo-cambio-clim%C3%A1tico.jpg"),
-    Regiones("La Libertad","https://elcomercio.pe/resizer/qQOmnlKQsSeLCb83LLmeY3VlnIc=/580x330/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/BUCL36J6JJECJJSP6FOW66AN3E.jpg"),
-    Regiones("Lambayeque","https://portal.andina.pe/EDPfotografia3/Thumbnail/2019/04/16/000579168W.jpg"),
-    Regiones("Lima Metropolitana","https://www.turiweb.pe/wp-content/uploads/2021/08/lima2-050821.jpg"),
-    Regiones("Lima (departamento)","https://www.ciudaris.com/blog/wp-content/uploads/consejos-comprar-departamento-lima.jpg"),
-    Regiones("Loreto","https://www.peru.travel/Contenido/Destino/Imagen/es/9/1.2/Principal/Loreto.jpg"),
-    Regiones("Madre de Dios","https://1.bp.blogspot.com/-JFo1NbuGXj4/WshEIEx2-TI/AAAAAAAAAws/64n5zQqpPZUba9l7pcivg9bQUk7_DxxwwCPcBGAYYCw/s640/Madre%2Bde%2BDios%2BPeru.jpg"),
-    Regiones("Moquegua","https://www.peru.travel/Contenido/Destino/Imagen/es/347/1.2/Principal/1.jpg"),
-    Regiones("Pasco","https://www.ytuqueplanes.com/imagenes/fotos/regiones/RC-BANNER-INT-Pasco.jpg"),
-    Regiones("Piura","https://turismoi.pe/uploads/photo/version3/photo_file/69804/piura.jpg"),
-    Regiones("Puno","https://www.peru.travel/Contenido/Destino/Imagen/pe/37/1.3/Principal/Los%20Uros.jpg"),
-    Regiones("San Martín","https://www.peru.travel/Contenido/Evento/Imagen/pe/431/1.1/Evento/San%20Mart%C3%ADn%20Portada.jpg"),
-    Regiones("Tacna","https://portal.andina.pe/EDPmedia/fotografia/2021/08/28/55136_paseocivico_tacna.jpg"),
-    Regiones("Tumbes","https://larepublica.pe/resizer/qBquSUeySt2THv_ZWEakaZ-sPrg=/1250x735/top/smart/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/73HJH4LFFBCMDHF2ZYHBZIMETE.jpg"),
-    Regiones("Ucayali","https://www.actualidadambiental.pe/wp-content/uploads/2019/03/pucallpa-ucayali-jaime-tranca.jpg")
 
+val regionesList = listOf(
+    Regiones("Amazonas", "https://www.fundacionaquae.org/wp-content/uploads/2019/07/rio-amazonas.jpg","rocoto"),
+    Regiones("Áncash","https://www.peru.travel/Contenido/Destino/Imagen/pe/26/1.3/Principal/Ancash.jpg","rocoto"),
+    Regiones("Apurimac","https://d1bvpoagx8hqbg.cloudfront.net/originals/apurimac-alturas-canones-naturales-fd503277fa8c8610123683d60f483e07.jpg","rocoto"),
+    Regiones("Arequipa","https://media.vogue.mx/photos/5e5c5b1f25623100081c437c/master/w_3000,h_1999,c_limit/Arequipa--paisaje.jpg","rocoto"),
+    Regiones("Ayacucho","https://denomades.s3.us-west-2.amazonaws.com/blog/wp-content/uploads/2020/09/13172228/arco-ayacucho.jpg","rocoto"),
+    Regiones("Cajamarca","https://www.regioncajamarca.gob.pe/media/portal/KJDIG/noticia/Plaza_de_Armas_WEB_1.jpg?r=1573668242","rocoto"),
+    Regiones("Callao","https://elcomercio.pe/resizer/MfFipsfIj-o5SN8l6-fDYGTRqKY=/980x0/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/EVFPRGZ4W5G5DPOSRSWL2PMUZE.jpg","rocoto"),
+    Regiones("Cusco","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/33/fc/ba/cusco.jpg?w=700&h=500&s=1","rocoto"),
+    Regiones("Huancavelica","https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Panor%C3%A1mica_de_Huancavelica.jpg/1200px-Panor%C3%A1mica_de_Huancavelica.jpg","rocoto"),
+    Regiones("Huánuco","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/4a/85/5e/dscf7752-snapseed-largejpg.jpg?w=500&h=400&s=1","rocoto"),
+    Regiones("Ica","https://www.peru.travel/Contenido/Destino/Imagen/es/5/1.2/Principal/SouthAmericaPeru2017_1010_180704-5912_AGP_HDR.jpg","rocoto"),
+    Regiones("Junín","https://la.network/wp-content/uploads/2019/05/huancayo-cambio-clim%C3%A1tico.jpg","rocoto"),
+    Regiones("La Libertad","https://elcomercio.pe/resizer/qQOmnlKQsSeLCb83LLmeY3VlnIc=/580x330/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/BUCL36J6JJECJJSP6FOW66AN3E.jpg","rocoto"),
+    Regiones("Lambayeque","https://portal.andina.pe/EDPfotografia3/Thumbnail/2019/04/16/000579168W.jpg","rocoto"),
+    Regiones("Lima Metropolitana","https://www.turiweb.pe/wp-content/uploads/2021/08/lima2-050821.jpg","rocoto"),
+    Regiones("Lima (departamento)","https://www.ciudaris.com/blog/wp-content/uploads/consejos-comprar-departamento-lima.jpg","rocoto"),
+    Regiones("Loreto","https://www.peru.travel/Contenido/Destino/Imagen/es/9/1.2/Principal/Loreto.jpg","rocoto"),
+    Regiones("Madre de Dios","https://1.bp.blogspot.com/-JFo1NbuGXj4/WshEIEx2-TI/AAAAAAAAAws/64n5zQqpPZUba9l7pcivg9bQUk7_DxxwwCPcBGAYYCw/s640/Madre%2Bde%2BDios%2BPeru.jpg","rocoto"),
+    Regiones("Moquegua","https://www.peru.travel/Contenido/Destino/Imagen/es/347/1.2/Principal/1.jpg","rocoto"),
+    Regiones("Pasco","https://www.ytuqueplanes.com/imagenes/fotos/regiones/RC-BANNER-INT-Pasco.jpg","rocoto"),
+    Regiones("Piura","https://turismoi.pe/uploads/photo/version3/photo_file/69804/piura.jpg","rocoto"),
+    Regiones("Puno","https://www.peru.travel/Contenido/Destino/Imagen/pe/37/1.3/Principal/Los%20Uros.jpg","rocoto"),
+    Regiones("San Martín","https://www.peru.travel/Contenido/Evento/Imagen/pe/431/1.1/Evento/San%20Mart%C3%ADn%20Portada.jpg","rocoto"),
+    Regiones("Tacna","https://portal.andina.pe/EDPmedia/fotografia/2021/08/28/55136_paseocivico_tacna.jpg","rocoto"),
+    Regiones("Tumbes","https://larepublica.pe/resizer/qBquSUeySt2THv_ZWEakaZ-sPrg=/1250x735/top/smart/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/73HJH4LFFBCMDHF2ZYHBZIMETE.jpg","rocoto"),
+    Regiones("Ucayali","https://www.actualidadambiental.pe/wp-content/uploads/2019/03/pucallpa-ucayali-jaime-tranca.jpg","rocoto")
 )
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RegionList(regionesList)
+            val NavController = rememberNavController()
+            NavHost(navController = NavController, startDestination = "regionlist" ){
+                composable( "regionlist" ){
+                    RegionList(regionesList, NavController)
+                }
+                composable(
+                    route = "recepies/{regionName}")
+                {   backStackEntry ->
+                        val regionId = backStackEntry.arguments?.getString("regionName")
+                    //el argumento no puede ser null
+                    requireNotNull(regionId)
+                    Recepies(regionId)
+                }
+            }
+
         }
     }
 }
+
+
+
 @Composable
-fun RegionCard(region:Regiones){
+fun RegionCard(region:Regiones,navController: NavController){
     Surface(shape = RoundedCornerShape(8.dp),
     elevation = 8.dp,
     modifier = Modifier.padding(8.dp)) {
@@ -88,14 +116,14 @@ fun RegionCard(region:Regiones){
             Row(modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceEvenly) {
                 Text(text = region.title, style = MaterialTheme.typography.h6)
-                SimpleButton(region = region.title)
+                SimpleButton(region = region.title,navController)
             }
 
         }
     }
 }
 @Composable
-fun SimpleButton(region:String) {
+fun SimpleButton(region:String,navController: NavController) {
    Button(onClick = { /* En esta seccion se envia a las vistas de recetas*/
         /*
          val intent = Intent(this, DisplayMessageActivity::class.java).apply {
@@ -104,24 +132,68 @@ fun SimpleButton(region:String) {
         startActivity(intent)
 
          */
+        navController.navigate("recepies/${region}")
    }) {
        Text(text = "Recetas")
    }
 }
 
 @Composable
-fun RegionList(regionList: List<Regiones>){
+fun RegionList(regionList: List<Regiones>,navController: NavController){
     LazyColumn( modifier = Modifier.fillMaxWidth()){
         items(regionList){
                 region ->
-                RegionCard(region = region)
+                RegionCard(region = region,navController)
 
         }
     }
 }
 
-@Preview
+@Composable
+fun Recepies(regionName:String){
+
+    Surface(shape = RoundedCornerShape(8.dp),
+        elevation = 8.dp,
+        modifier = Modifier.padding(8.dp)) {
+        Column(modifier = Modifier.padding(16.dp),
+        ) {
+        for (region in regionesList) {
+            if (region.title.equals(regionName)) {
+                val modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(8.dp))
+                val painter = rememberAsyncImagePainter(model = region.url)
+                val estado = painter.state
+                Image(
+                    painter = painter,
+                    contentDescription = null,
+                    modifier = modifier,
+                    contentScale = ContentScale.Crop
+                )
+                if (estado is AsyncImagePainter.State.Loading) {
+                    CircularProgressIndicator()
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(
+                        text = region.title + "\n" + region.recepies,
+                        style = MaterialTheme.typography.h6
+                    )
+                }
+
+            }
+        }
+        }
+
+    }
+}
+
+/*@Preview
 @Composable
 fun RegionesPreview(){
     RegionCard(regionesList[0])
-}
+}*/

cf462e84b2a21a03893b4627b4c27bb5eab1ed06
AlexisAH <aarroyoh@unsa.edu.pe>
2022-05-12 22:47:50 -0500

    Datos de recetas

--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -36,32 +36,33 @@ data class Regiones(
 )
 
 val regionesList = listOf(
-    Regiones("Amazonas", "https://www.fundacionaquae.org/wp-content/uploads/2019/07/rio-amazonas.jpg","rocoto"),
-    Regiones("Áncash","https://www.peru.travel/Contenido/Destino/Imagen/pe/26/1.3/Principal/Ancash.jpg","rocoto"),
-    Regiones("Apurimac","https://d1bvpoagx8hqbg.cloudfront.net/originals/apurimac-alturas-canones-naturales-fd503277fa8c8610123683d60f483e07.jpg","rocoto"),
-    Regiones("Arequipa","https://media.vogue.mx/photos/5e5c5b1f25623100081c437c/master/w_3000,h_1999,c_limit/Arequipa--paisaje.jpg","rocoto"),
-    Regiones("Ayacucho","https://denomades.s3.us-west-2.amazonaws.com/blog/wp-content/uploads/2020/09/13172228/arco-ayacucho.jpg","rocoto"),
-    Regiones("Cajamarca","https://www.regioncajamarca.gob.pe/media/portal/KJDIG/noticia/Plaza_de_Armas_WEB_1.jpg?r=1573668242","rocoto"),
-    Regiones("Callao","https://elcomercio.pe/resizer/MfFipsfIj-o5SN8l6-fDYGTRqKY=/980x0/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/EVFPRGZ4W5G5DPOSRSWL2PMUZE.jpg","rocoto"),
-    Regiones("Cusco","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/33/fc/ba/cusco.jpg?w=700&h=500&s=1","rocoto"),
-    Regiones("Huancavelica","https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Panor%C3%A1mica_de_Huancavelica.jpg/1200px-Panor%C3%A1mica_de_Huancavelica.jpg","rocoto"),
-    Regiones("Huánuco","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/4a/85/5e/dscf7752-snapseed-largejpg.jpg?w=500&h=400&s=1","rocoto"),
-    Regiones("Ica","https://www.peru.travel/Contenido/Destino/Imagen/es/5/1.2/Principal/SouthAmericaPeru2017_1010_180704-5912_AGP_HDR.jpg","rocoto"),
-    Regiones("Junín","https://la.network/wp-content/uploads/2019/05/huancayo-cambio-clim%C3%A1tico.jpg","rocoto"),
-    Regiones("La Libertad","https://elcomercio.pe/resizer/qQOmnlKQsSeLCb83LLmeY3VlnIc=/580x330/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/BUCL36J6JJECJJSP6FOW66AN3E.jpg","rocoto"),
-    Regiones("Lambayeque","https://portal.andina.pe/EDPfotografia3/Thumbnail/2019/04/16/000579168W.jpg","rocoto"),
-    Regiones("Lima Metropolitana","https://www.turiweb.pe/wp-content/uploads/2021/08/lima2-050821.jpg","rocoto"),
-    Regiones("Lima (departamento)","https://www.ciudaris.com/blog/wp-content/uploads/consejos-comprar-departamento-lima.jpg","rocoto"),
-    Regiones("Loreto","https://www.peru.travel/Contenido/Destino/Imagen/es/9/1.2/Principal/Loreto.jpg","rocoto"),
-    Regiones("Madre de Dios","https://1.bp.blogspot.com/-JFo1NbuGXj4/WshEIEx2-TI/AAAAAAAAAws/64n5zQqpPZUba9l7pcivg9bQUk7_DxxwwCPcBGAYYCw/s640/Madre%2Bde%2BDios%2BPeru.jpg","rocoto"),
-    Regiones("Moquegua","https://www.peru.travel/Contenido/Destino/Imagen/es/347/1.2/Principal/1.jpg","rocoto"),
-    Regiones("Pasco","https://www.ytuqueplanes.com/imagenes/fotos/regiones/RC-BANNER-INT-Pasco.jpg","rocoto"),
-    Regiones("Piura","https://turismoi.pe/uploads/photo/version3/photo_file/69804/piura.jpg","rocoto"),
-    Regiones("Puno","https://www.peru.travel/Contenido/Destino/Imagen/pe/37/1.3/Principal/Los%20Uros.jpg","rocoto"),
-    Regiones("San Martín","https://www.peru.travel/Contenido/Evento/Imagen/pe/431/1.1/Evento/San%20Mart%C3%ADn%20Portada.jpg","rocoto"),
-    Regiones("Tacna","https://portal.andina.pe/EDPmedia/fotografia/2021/08/28/55136_paseocivico_tacna.jpg","rocoto"),
-    Regiones("Tumbes","https://larepublica.pe/resizer/qBquSUeySt2THv_ZWEakaZ-sPrg=/1250x735/top/smart/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/73HJH4LFFBCMDHF2ZYHBZIMETE.jpg","rocoto"),
-    Regiones("Ucayali","https://www.actualidadambiental.pe/wp-content/uploads/2019/03/pucallpa-ucayali-jaime-tranca.jpg","rocoto")
+    Regiones("Amazonas", "https://www.fundacionaquae.org/wp-content/uploads/2019/07/rio-amazonas.jpg","Pescado machucado,\n Patarasca de pirarucú,\n Sancocho huitoto, \nCaldo de pescado, \nPatarasca de mojojoi"),
+    Regiones("Áncash","https://www.peru.travel/Contenido/Destino/Imagen/pe/26/1.3/Principal/Ancash.jpg","Llunca cashqui, \nTamales de Ancash, \nKuchi kanka o cuchi canca,\n Picante de chocho, \nPatasca Ancashina,\n Caldo de cuy o Aca cashqui"),
+    Regiones("Apurimac","https://d1bvpoagx8hqbg.cloudfront.net/originals/apurimac-alturas-canones-naturales-fd503277fa8c8610123683d60f483e07.jpg","La Huatía,\n Chicharron de Cerdo, \nCuy relleno, \nEstofado de gallina, \nKapchi de chuño, \nCancacho o Kankacho"),
+    Regiones("Arequipa","https://media.vogue.mx/photos/5e5c5b1f25623100081c437c/master/w_3000,h_1999,c_limit/Arequipa--paisaje.jpg","Rocoto Relleno, \nChupe de camarones, \nAdobo de chancho, \nPastel de papa,\n Cuy Chactado"),
+    Regiones("Ayacucho","https://denomades.s3.us-west-2.amazonaws.com/blog/wp-content/uploads/2020/09/13172228/arco-ayacucho.jpg","Teqte ayacuchano, Patachi,\n Qapchi, \nCuy chactado ayacuchano,\n Puchero   "),
+    Regiones("Cajamarca","https://www.regioncajamarca.gob.pe/media/portal/KJDIG/noticia/Plaza_de_Armas_WEB_1.jpg?r=1573668242","Caldo Verde, \nQuesillo en miel,\n Chicharrón con mote, \nSopa de chochoca con cecina, \nCuy con mote"),
+    Regiones("Callao","https://elcomercio.pe/resizer/MfFipsfIj-o5SN8l6-fDYGTRqKY=/980x0/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/EVFPRGZ4W5G5DPOSRSWL2PMUZE.jpg","CHORITOS A LA CHALACA, \nMUCHAME DE ATÚN"),
+    Regiones("Cusco","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/33/fc/ba/cusco.jpg?w=700&h=500&s=1","Chiri Uchu, \nTrucha Frita,\n Chairo,\n Sopa de Quinua, \nCuy al horno, \nKapchi de setas"),
+    Regiones("Huancavelica","https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Panor%C3%A1mica_de_Huancavelica.jpg/1200px-Panor%C3%A1mica_de_Huancavelica.jpg","Sopa de mondongo,\n Carnero al palo, \nPatachi, \nTiqi, \nPachamanca"),
+    Regiones("Huánuco","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/4a/85/5e/dscf7752-snapseed-largejpg.jpg?w=500&h=400&s=1","Locro de gallina,\n Pachamanca de Chancho, \nPicante de cuy,\n Juane de gallina, \nTacacho con cecina"),
+    Regiones("Ica","https://www.peru.travel/Contenido/Destino/Imagen/es/5/1.2/Principal/SouthAmericaPeru2017_1010_180704-5912_AGP_HDR.jpg","Sopa seca,\n Carapulcra,\n Chupe de pallares,\nLas tejas"),
+    Regiones("Junín","https://la.network/wp-content/uploads/2019/05/huancayo-cambio-clim%C3%A1tico.jpg","Trucha frita, \nPapa a la huancaína, \nPicante de cuy,\nCuy chactado, \nHuallpa chupe o chupe de gallina"),
+    Regiones("La Libertad","https://elcomercio.pe/resizer/qQOmnlKQsSeLCb83LLmeY3VlnIc=/580x330/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/BUCL36J6JJECJJSP6FOW66AN3E.jpg","Shámbar,\nCeviche de conchas negras, \nSopa teóloga,\nSeco de cabrito a la norteña"),
+    Regiones("Lambayeque","https://portal.andina.pe/EDPfotografia3/Thumbnail/2019/04/16/000579168W.jpg","Arroz con pato,\n Ceviche de pato, \nCabrito a la norteña,\nTortilla de raya,\nChiringuito"),
+    Regiones("Lima Metropolitana","https://www.turiweb.pe/wp-content/uploads/2021/08/lima2-050821.jpg","Anticucho,\n Salchipapa"),
+    Regiones("Lima (departamento)","https://www.ciudaris.com/blog/wp-content/uploads/consejos-comprar-departamento-lima.jpg","Anticucho, \nSalchipapa"),
+    Regiones("Loreto","https://www.peru.travel/Contenido/Destino/Imagen/es/9/1.2/Principal/Loreto.jpg","Frejoles guisados con pollo,\n Juene de higado, \nSalpicon de pollo"),
+    Regiones("Madre de Dios","https://1.bp.blogspot.com/-JFo1NbuGXj4/WshEIEx2-TI/AAAAAAAAAws/64n5zQqpPZUba9l7pcivg9bQUk7_DxxwwCPcBGAYYCw/s640/Madre%2Bde%2BDios%2BPeru.jpg","Seco de pollo, \nSaltado de riñon, Caigua rellena, \nPollo en salsa de mani"),
+    Regiones("Moquegua","https://www.peru.travel/Contenido/Destino/Imagen/es/347/1.2/Principal/1.jpg","La Chacharrada,Patasca Moqueguana o Caldo de Mondongo,\n Cuy Frito y Picante de Cuy,\n Moqueguano de Camarón,\n Sudado de Machas"),
+    Regiones("Pasco","https://www.ytuqueplanes.com/imagenes/fotos/regiones/RC-BANNER-INT-Pasco.jpg","Pescado frito con frejol, \nCau cau, \nRiñon de saltado,\n Carne saltada con arveja"),
+    Regiones("Piura","https://turismoi.pe/uploads/photo/version3/photo_file/69804/piura.jpg","La malarrabia, \nSeco de cabrito a la norteña, \nPescado pasadito por agua ,\n" +
+            "Sudado de cachema\n"),
+    Regiones("Puno","https://www.peru.travel/Contenido/Destino/Imagen/pe/37/1.3/Principal/Los%20Uros.jpg","Pesque de quinua,\n El Chairo,\n Chupe de quinua,\n Chicharrón de alpaca, \nSancochado puneño"),
+    Regiones("San Martín","https://www.peru.travel/Contenido/Evento/Imagen/pe/431/1.1/Evento/San%20Mart%C3%ADn%20Portada.jpg","Avispa Juane,\nPatarashca, \nChipado de pescado,\nRumo-Api y Sara-Api"),
+    Regiones("Tacna","https://portal.andina.pe/EDPmedia/fotografia/2021/08/28/55136_paseocivico_tacna.jpg","Picante a la tacneña,\nAdobo tacneño,\nChoclo con queso,\nChicharrón de chancho con maíz tostado,\nCazuela tacneña"),
+    Regiones("Tumbes","https://larepublica.pe/resizer/qBquSUeySt2THv_ZWEakaZ-sPrg=/1250x735/top/smart/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/73HJH4LFFBCMDHF2ZYHBZIMETE.jpg","Ceviche de conchas negras,\nChilcano de pescado,\nAlbóndiga de pulpa de cangrejo,\nChupe de cangrejo,\nMajarisco tumbesino"),
+    Regiones("Ucayali","https://www.actualidadambiental.pe/wp-content/uploads/2019/03/pucallpa-ucayali-jaime-tranca.jpg","El Juane de Gallina,\nTacacho con cecina,\nChonta,\nPatarashca,\n Picadillo de Paiche")
 )
 
 
@@ -180,7 +181,7 @@ fun Recepies(regionName:String){
                     horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
                     Text(
-                        text = region.title + "\n" + region.recepies,
+                        text = "Recetas de "+region.title + "\n" + region.recepies,
                         style = MaterialTheme.typography.h6
                     )
                 }
